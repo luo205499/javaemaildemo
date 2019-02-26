1a05b0a2496fa07836d0ee33b9dd14ea7ab01ec8
luochunlong <657050518@qq.com>
2019-02-26 09:29:21 +0800

    Initial commit

---
+++ src/main/java/com/javaemaildemo/JavaemaildemoApplication.java
@@ -0,0 +1,13 @@
+package com.javaemaildemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaemaildemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaemaildemoApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/javaemaildemo/service/IEmailService.java
@@ -0,0 +1,26 @@
+package com.javaemaildemo.service;
+
+import java.util.List;
+
+public interface IEmailService {
+	/**
+	 * 
+	* @Description 发送简单纯文字邮件
+	* @param to 收件人地址
+	* @param subject 邮件主题
+	* @param content 邮件内容<br>
+	* @return boolean 成功返回true，失败返回false
+	 */
+	 boolean sendAttachmentMail(String to, String subject, String content);
+	
+	/**
+	 * 
+	* @Description 发送带单个或多个附件的邮件
+	* @param to 收件人地址
+	* @param subject 邮件主题
+	* @param content 邮件内容
+	* @param filepath 包含附件路径地址的字符串数组
+	* @return boolean 功返回true，失败返回false
+	 */
+	 boolean sendAttachmentMail(String to, String subject, String content, List filepath);
+}
